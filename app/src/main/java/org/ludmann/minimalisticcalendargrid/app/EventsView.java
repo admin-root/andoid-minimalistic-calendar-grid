6b40e5eafaae81c7ed9ec013e78828cc54f33d53
Cornelius Ludmann <dev@cornelius-ludmann.de>
2014-04-27 18:29:06 +0200

    more data in events list (new arrayadapter)

--- app/src/main/java/org/ludmann/minimalisticcalendargrid/app/EventsView.java
+++ app/src/main/java/org/ludmann/minimalisticcalendargrid/app/EventsView.java
@@ -2,15 +2,22 @@ package org.ludmann.minimalisticcalendargrid.app;
 
 import android.app.Activity;
 import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.CalendarContract;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -32,7 +39,8 @@ public class EventsView extends Activity implements LoaderManager.LoaderCallback
             CalendarContract.Events.ALL_DAY,            // 5
             CalendarContract.Events.DELETED,            // 6
             CalendarContract.Events.VISIBLE,            // 7
-            CalendarContract.Events.DISPLAY_COLOR       // 8
+            CalendarContract.Events.DISPLAY_COLOR,        // 8
+            CalendarContract.Events.CALENDAR_DISPLAY_NAME   // 9
     };
     /**
      * The selection string for the events.
@@ -73,6 +81,18 @@ public class EventsView extends Activity implements LoaderManager.LoaderCallback
 
         ((TextView) findViewById(R.id.date)).setText(DateFormat.getDateInstance(DateFormat.LONG).format(calendar.getTime()));
 
+        ((ListView) findViewById(R.id.eventsListView)).setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Event event = (Event) adapterView.getItemAtPosition(position);
+
+                Intent calIntent = new Intent(Intent.ACTION_VIEW);
+                calIntent.setData(ContentUris.withAppendedId(CalendarContract.Events.CONTENT_URI, event.id));
+                startActivity(calIntent);
+            }
+        });
+
+
 
         calendar.setTimeZone(TimeZone.getTimeZone("UTC"));
 
@@ -95,9 +115,6 @@ public class EventsView extends Activity implements LoaderManager.LoaderCallback
 
         //getLoaderManager().initLoader((int) (startOfDay / 1000L), bundle, this);
         getLoaderManager().initLoader(0, bundle, this);
-
-
-        // TODO: Display Date
     }
 
 
@@ -151,15 +168,25 @@ public class EventsView extends Activity implements LoaderManager.LoaderCallback
     @Override
     public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
 
-        String[] eventTitles = new String[cursor.getCount()];
+        Event[] events = new Event[cursor.getCount()];
 
         int i = 0;
         while (cursor.moveToNext()) {
-            eventTitles[i] = cursor.getString(1);
+            events[i] = new Event();
+            events[i].id = cursor.getLong(0);
+            events[i].title = cursor.getString(1);
+            events[i].location = cursor.getString(2);
+            events[i].dtstart = cursor.getLong(3);
+            events[i].dtend = cursor.getLong(4);
+            events[i].allDay = cursor.getInt(5) == 0 ? false : true;
+            events[i].deleted = cursor.getInt(6) == 0 ? false : true;
+            events[i].visible = cursor.getInt(7) == 0 ? false : true;
+            events[i].displayColor = cursor.getInt(8);
+            events[i].calendarDisplayName = cursor.getString(9);
             ++i;
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eventTitles);
+        ListAdapter adapter = new EventsAdapter(this, events);
 
         ((ListView) findViewById(R.id.eventsListView)).setAdapter(adapter);
 
@@ -168,5 +195,109 @@ public class EventsView extends Activity implements LoaderManager.LoaderCallback
     @Override
     public void onLoaderReset(Loader<Cursor> cursorLoader) {
 
+    }
+
+    /**
+     * Holds the event in the ArrayAdapter.
+     * <p/>
+     * To make it as simple as possible I've passed on the getters and setters.
+     */
+    private class Event {
+        public long id;
+        public String title;
+        public String location;
+        public long dtstart;
+        public long dtend;
+        public boolean allDay;
+        public boolean deleted;
+        public boolean visible;
+        public int displayColor;
+        public String calendarDisplayName;
+    }
+
+
+    private class EventsAdapter extends ArrayAdapter<Event> {
+        public EventsAdapter(Context context, Event[] events) {
+            super(context, R.layout.event_item, events);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Get the data item for this position
+            Event event = getItem(position);
+
+            // Check if an existing view is being reused, otherwise inflate the view
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.event_item, parent, false);
+            }
+
+            TextView textViewTime = (TextView) convertView.findViewById(R.id.time);
+            TextView textViewTitle = (TextView) convertView.findViewById(R.id.title);
+            TextView textViewLocation = (TextView) convertView.findViewById(R.id.location);
+            TextView textViewExtraInfo = (TextView) convertView.findViewById(R.id.extrainfo);
+
+
+            Calendar start = Calendar.getInstance();
+            // dtstart is in UTC
+            start.setTimeInMillis(event.dtstart - TimeZone.getDefault().getOffset(event.dtstart));
+            Calendar end = Calendar.getInstance();
+            // dtend is in UTC
+            end.setTimeInMillis(event.dtend - TimeZone.getDefault().getOffset(event.dtend));
+
+
+            Calendar startOfDay = Calendar.getInstance();
+            //startOfDay.setTimeZone(TimeZone.getTimeZone("UTC"));
+            startOfDay.set(year, month, day, 0, 0, 0);
+            startOfDay.set(Calendar.MILLISECOND, 0);
+            Calendar endOfDay = Calendar.getInstance();
+            //endOfDay.setTimeZone(TimeZone.getTimeZone("UTC"));
+            endOfDay.set(year, month, day, 23, 59, 59);
+            endOfDay.set(Calendar.MILLISECOND, endOfDay.getActualMaximum(Calendar.MILLISECOND));
+            endOfDay.add(Calendar.MILLISECOND, 1);
+            boolean onlyThisDay = !start.before(startOfDay) && !end.after(endOfDay);
+            //System.out.println("start before startofday? " + start.before(startOfDay) + " " + start.getTimeInMillis() + " " + startOfDay.getTimeInMillis());
+            //System.out.println("end after endofday? " + end.before(endOfDay) + " " + end.getTimeInMillis() + " " + endOfDay.getTimeInMillis());
+
+            String timeText;
+            if (event.allDay) {
+                if (onlyThisDay) {
+                    timeText = null;
+                } else {
+                    DateFormat dateFormat = DateFormat.getDateInstance();
+                    //DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+
+                    // Because all day events end at 00:00 of the next day ...
+                    end.add(Calendar.MILLISECOND, -1);
+                    timeText = dateFormat.format(start.getTime()) + " – " + dateFormat.format(end.getTime());
+                }
+            } else {
+                DateFormat dateFormat;
+                if (onlyThisDay) {
+                    dateFormat = DateFormat.getTimeInstance(DateFormat.SHORT);
+                } else {
+                    dateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
+
+                }
+                timeText = dateFormat.format(start.getTime()) + " – " + dateFormat.format(end.getTime());
+            }
+
+
+            if (timeText == null) {
+                textViewTime.setVisibility(View.GONE);
+            } else {
+                textViewTime.setText(timeText);
+            }
+            textViewTitle.setText(event.title);
+            textViewTitle.setTextColor(event.displayColor);
+            if (event.location == null || event.location.trim().length() == 0) {
+                textViewLocation.setVisibility(View.GONE);
+            } else {
+                textViewLocation.setText(event.location);
+            }
+
+            textViewExtraInfo.setText(getString(R.string.calendar_display_name) + ": " + event.calendarDisplayName);
+
+            return convertView;
+        }
     }
 }
